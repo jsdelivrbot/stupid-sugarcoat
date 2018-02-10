d53644165794ec657d0356795d3a7187b0b5e439
Jonas Johansson <jns.johansson@gmail.com>
2018-02-10 16:59:47 +0100

    -

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let source = document.createElement("source");
 
 audio.autoplay = true;
 
-source.src = "honey.mp3";
+source.src = "https://rawgit.com/jonasjohansson/sugarcoat/master/honey.mp3";
 source.type = "audio/mp3";
 
 audio.appendChild(source);
